a958c265b5fe40b55d23ce68a565cdafffbc826a
a kadyrova <gm808035@gmail.com>
2020-03-31 23:41:57 +0600

    add packages

---
+++ build/app.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var app = express();
+var port = 3000;
+app.get('/', function (req, res) {
+    res.send('The sedulous hyena ate the antelope!');
+});
+app.listen(port, function (err) {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log("server is listening on " + port);
+});
+//# sourceMappingURL=app.js.map
